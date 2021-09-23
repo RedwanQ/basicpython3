212c415efc2f8458d8e08e7e2142df8290d48dfe
Redwan Diriye <ridwanmohamedco@outlook.com>
2021-09-23 08:59:46 -0500

    done

--- math.py
+++ math.py
@@ -1,1 +1,17 @@
 import math
+# 
+# width = int(input('Enter the Width'))
+# length = int(input('Enter the Length'))
+
+#  area = width * length
+
+#  print(f'the square footage of a house is {area} ')
+
+
+# Circumference of circle
+
+radius = float(input('Enter the raduis of your circle'))
+
+C = 2*math.pi*radius
+
+print(f'Cirumference of circle is {C}')
